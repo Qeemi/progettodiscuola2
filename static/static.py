e4d91d2c7133edabb4a9e51a8facd4666dcf8390
Giacomo <giacofaraglia@gmail.com>
2025-09-27 15:14:59 +0200

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,5 +1,6 @@
 # static.py
 
 HTML = """
-dajeeeeeeeeeeeeeeeee nnamo al mare a fa casino a vede il sole e a guarda i pesci che fanno le bolle senza la bocca che poi non so se vedo il sole perchè anche tu sei un po maldestro
+dajeeeeeeeeeeeeeeeee nnamo al mare a fa casino a vede il sole e a guarda i pesci che fanno le 
+bolle senza la bocca che poi non so se vedo il sole perchè anche tu sei un po maldestro
 """
